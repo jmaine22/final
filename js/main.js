b8470117b9ac175f9da7b84086288a398596e5aa
jmaine22 <jmaine22@gmail.com>
2021-06-12 01:15:12 -0700

    final

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ window.addEventListener("load", validate);
 function validate() {
     let myform = document.getElementById("deliveryInfo").elements;
     let orderStart = document.getElementById('startPizza');
-    orderStart.addEventListener("click", () => {
+    orderStart.addEventListener("click", (e) => {
         for(i = 0; i < myform.length; i++){
-            if (myform[i].value == ""){
-                alert("Missing require information!");
+            if (myform[i].validity){
+                //alert("Missing require information!");
+                let myInput = myform[i].elements;
+                console.log(myInput);
+                e.preventDefault();
+                //myInput.setCustomValidity('You need to fill this out');
+                // input.reportValidity();
             }
         }
     });
