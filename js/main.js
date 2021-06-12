f2eb8132c5a642448d22934c9863f119783048c9
jmaine22 <jmaine22@gmail.com>
2021-06-11 21:49:01 -0700

    final

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ window.addEventListener("load", validate);
 function validate() {
     let myform = document.getElementById("deliveryInfo").elements;
     let orderStart = document.getElementById('startPizza');
-    
-    for(i = 0; i < myform.length; i++){
-        if (myform[i].value == ""){
-            alert("Missing require information!");
+    orderStart.addEventListener("click", () => {
+        for(i = 0; i < myform.length; i++){
+            if (myform[i].value == ""){
+                alert("Missing require information!");
+            }
         }
-    }
+    });
 }
