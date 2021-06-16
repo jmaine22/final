abbb0450914fc95419cd6f7ba34f18deab3939e0
jmaine22 <jmaine22@gmail.com>
2021-06-16 01:28:11 -0700

    final

--- js/main.js
+++ js/main.js
@@ -3,16 +3,21 @@ window.addEventListener("load", validate);
 function validate() {
     let myform = document.getElementById("deliveryInfo").elements;
     let orderStart = document.getElementById('startPizza');
+    jQuery(document).ready(function ($) {
+        $(orderStart).removeClass('disabled');
+    });
     orderStart.addEventListener("click", (e) => {
         for(i = 0; i < myform.length; i++){
             if (myform[i].validity){
-                document.getElementById('startPizza').tabIndex = -1;
-                e.preventDefault();
+                jQuery(document).ready(function ($) {
+                    $(orderStart).addClass('disabled');
+                });
                 //alert("Missing require information!");
-                let myInput = myform[i];
-                console.log(myInput);
                 //myInput.setCustomValidity('You need to fill this out');
                 // input.reportValidity();
+            }
+            else {
+                
             }
         }
     });
